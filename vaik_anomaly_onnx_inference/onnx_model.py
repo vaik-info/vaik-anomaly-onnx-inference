71e3027cb637cb24d85d97c838d4846d6767824d
vaik-info <info@vaikobo.com>
2023-03-20 13:58:18 +0900

    first commit

--- vaik_anomaly_onnx_inference/onnx_model.py
+++ vaik_anomaly_onnx_inference/onnx_model.py
@@ -1,6 +1,6 @@
 from typing import List, Dict, Tuple
 import onnxruntime as rt
-from PIL import Image
+from PIL import Image, ImageOps
 import numpy as np
 
 
@@ -31,7 +31,7 @@ class OnnxModel:
             batch = resize_input_tensor[index:index + batch_size, :, :, :]
             batch_pad = np.zeros(((batch_size, ) + self.model_input_shape[1:]), dtype=np.float32)
             batch_pad[:batch.shape[0], :, :, :] = batch
-            raw_pred = self.model.run([self.output_name], {self.input_name: batch_pad})
+            raw_pred = self.model.run([self.output_name], {self.input_name: batch_pad})[0]
             output_tensor[index:index + batch.shape[0], :] = np.stack(raw_pred[:batch.shape[0]], axis=0)
         return output_tensor
 
@@ -50,23 +50,23 @@ class OnnxModel:
         if not np.issubdtype(input_image.dtype, np.uint8):
             raise ValueError(f'dtype mismatch expected: {np.uint8}, actual: {input_image.dtype}')
 
-        output_image = np.zeros((*resize_input_shape, input_image.shape[2]),
-                                dtype=input_image.dtype)
         pil_image = Image.fromarray(input_image)
-        x_ratio, y_ratio = resize_input_shape[1] / pil_image.width, resize_input_shape[0] / pil_image.height
-        if x_ratio < y_ratio:
-            resize_size = (resize_input_shape[1], round(pil_image.height * x_ratio))
-        else:
-            resize_size = (round(pil_image.width * y_ratio), resize_input_shape[0])
-        resize_pil_image = pil_image.resize(resize_size)
+        width, height = pil_image.size
+
+        scale = min(resize_input_shape[0] / height, resize_input_shape[1] / width)
+
+        resize_width = int(width * scale)
+        resize_height = int(height * scale)
+
+        resize_pil_image = pil_image.resize((resize_width, resize_height))
+        padding_bottom, padding_right = resize_input_shape[0] - resize_height, resize_input_shape[1] - resize_width
+        resize_pil_image = ImageOps.expand(resize_pil_image, (0, 0, padding_right, padding_bottom), fill=0)
         resize_image = np.array(resize_pil_image)
-        output_image[:resize_image.shape[0], :resize_image.shape[1], :] = resize_image
-        return output_image
+        return resize_image
 
     def __output_parse(self, pred: np.ndarray) -> List[Dict]:
         output_dict_list = []
-        pred_index = np.argsort(-pred, axis=-1)
         for index in range(pred.shape[0]):
-            output_dict = {'score': np.mean(pred[index][pred_index[index]])}
+            output_dict = {'score': np.mean(pred[index])}
             output_dict_list.append(output_dict)
         return output_dict_list
